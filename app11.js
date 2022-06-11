e70ad0576825df340a187ea5f42026f59320d9d6
Tyler <60554951+softwaresuave@users.noreply.github.com>
2022-06-10 20:31:06 -0500

    Add files via upload

---
+++ app11.js
@@ -0,0 +1,28 @@
+
+const jokes = document.querySelector('#jokes');
+const button = document.querySelector('button');
+
+const addNewJoke = async () => {
+    const jokeText = await getDadJoke();
+    // console.log(jokeText)
+    const newLI = document.createElement('LI');
+    newLI.append(jokeText);
+    jokes.append(newLI)
+
+
+
+
+}
+const getDadJoke = async () => {
+    const config = { headers: { Accept: 'application/json' } }
+    const res = await axios.get('https://icanhazdadjoke.com/', config)
+    return res.data.joke
+
+}
+
+button.addEventListener('click', addNewJoke)
+
+
+
+
+
